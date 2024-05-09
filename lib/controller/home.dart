8401862b2343e86a5b2e1fb3188cb06e945ff4b1
Christopher <christopher.lessirard@gmail.com>
2024-05-09 13:03:29 +0200

    Home UI base

---
+++ lib/controller/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:musicplayer/model/bar_choice.dart';
+
+//Home page
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int currentIndex=0;
+  List<BarChoice> items= [
+    BarChoice(label: "Music", iconData: Icons.music_note, page: Container()),
+    BarChoice(label: "Favorites", iconData: Icons.whatshot, page: Container()),
+    BarChoice(label: "Search", iconData: Icons.search, page: Container()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Spacer(),
+                  Icon(Icons.radio),
+                  Icon(Icons.doorbell),
+                  Icon(Icons.settings)
+                ],
+              ),
+              items[currentIndex].titleforAppBar
+            ],
+          ),
+        )),
+      ),
+      body: Center(
+        child: Text("App"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: items.map((i) => i.item).toList(),
+        onTap: barTapped,
+      ),
+    );
+  }
+
+  barTapped(int index){
+    setState(() {
+      currentIndex=index;
+    });
+  }
+}
---
+++ lib/model/bar_choice.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BarChoice {
+  String label;
+  IconData iconData;
+  Widget page;
+
+  BarChoice({required this.label, required this.iconData, required this.page});
+
+  Icon get icon => Icon(iconData);
+
+  BottomNavigationBarItem get item =>
+      BottomNavigationBarItem(icon: icon, label: label);
+
+  Text get titleforAppBar => Text(
+        label,
+        style: GoogleFonts.signika(fontSize: 30),
+      );
+}
